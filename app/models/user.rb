0d3d79da1311fd520c9b26d9b04579104b9ac90d
Khan <Talhaakhan@pm.me>
2021-01-13 15:15:19 +0800

    Form for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,5 @@ class User < ApplicationRecord
   validates :email, presence: true, 
     length: { maximum:25 }, uniqueness: { case_sensitive: false },
     format: {with: VALID_EMAIL_REGEX }
+
 end
