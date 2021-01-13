5efc2cd6098323a43daa8c6d5ed18b4631450c04
Khan <Talhaakhan@pm.me>
2021-01-13 15:32:18 +0800

    User form created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user= User.new(user_params)
+    if @user.save
+      flash[:notice]= "Welcome to the club, #{@user.username}"  
+      redirect_to cars_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password_digest)
   end
 
 end
