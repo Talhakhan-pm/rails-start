4981ed05d88ef3d14fb987ed6315bd9e8c830dc5
Khan <Talhaakhan@pm.me>
2021-01-13 13:26:57 +0800

    Okay boss

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  before_save {self.email= email.downcase}
+  before_save {self.username= username.downcase}
   has_many :cars
   validates :username, presence: true, 
     uniqueness: { case_sensitive: false }, length: {minimum:3, maximum:25 }
