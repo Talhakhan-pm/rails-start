d7974bb583f1475e45fc6670506057206961db03
Khan <Talhaakhan@pm.me>
2021-01-14 17:16:32 +0800

    Show user updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,19 +4,37 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user= User.new(user_params)
+    @user = User.new(user_params)
     if @user.save
-      flash[:notice]= "Welcome to the club, #{@user.username}"  
+      flash[:notice] = "Welcome to Fiscal Park #{@user.username}, you have successfully signed up"
       redirect_to cars_path
     else
       render 'new'
     end
   end
 
+  def edit
+    @user= User.find(params[:id])
+  end
+  def update
+    @user=User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice]= "Your User was updated"
+      redirect_to cars_path
+    else 
+      render 'edit'
+    end
+  end
+
+  def show
+    @user= User.find(params[:id])
+    @car= @user.cars
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password_digest)
+    params.require(:user).permit(:username, :email, :password)
   end
 
-end
+  end
